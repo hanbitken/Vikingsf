ea069c0d97df591aefc1f27aabedbd4b31911b73
kenndy <127312428+hanbitken@users.noreply.github.com>
2025-06-03 11:01:02 +0700

    Revert "design home page , login register , blm rapi hanya testing ke endpoint"
    This reverts commit feb6c2097df03355721b06d374a064166eda5e86.

--- src/assets/Components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-      <p>&copy; 2025 Vikings News. All rights reserved.</p>
-    </footer>
-  );
-}
