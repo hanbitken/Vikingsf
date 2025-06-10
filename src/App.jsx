7a6f586a15ee770c85a75adb590cf5b1bde4e636
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-10 12:11:58 +0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Components/home';
-import Header from './Components/header';
-import Footer from './Components/footer';
-import Login from './Components/Login';
-import GameInfo from './Pages/GameInfo/gameinfo';
-import ServerInfo from './Pages/GameInfo/serverinfo';
-import QuestInfo from './Pages/GameInfo/questinfo';
-import Rules from './Pages/GameInfo/rules';
-import MapInfo from './Pages/GameInfo/mapinfo'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./Pages/home";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import Login from "./Components/Login";
+import ServerInfo from "./Pages/GameInfo/serverinfo";
+import QuestInfo from "./Pages/GameInfo/questinfo";
+import Rules from "./Pages/GameInfo/rules";
+import MapInfo from "./Pages/GameInfo/mapinfo";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -24,10 +19,9 @@ function App() {
       <Header />
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
           {/* Tambahkan route lain jika perlu */}
           <Route path="/login" element={<Login />} />
-          <Route path="/gameinfo" element={<GameInfo />} />
+          <Route path="/" element={<Home />} />
           <Route path="/gameinfo/server" element={<ServerInfo />} />
           <Route path="/gameinfo/quest" element={<QuestInfo />} />
           <Route path="/gameinfo/map" element={<MapInfo />} />
