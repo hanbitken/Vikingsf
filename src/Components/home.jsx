56b25191066420d5a604fb17a4e142ce1b615808
kenndy <127312428+hanbitken@users.noreply.github.com>
2025-06-18 16:10:13 +0700

    cekpoint

---
+++ src/Components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Hero from './hero';
+import NewsSection from './NewsSection';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <NewsSection />
+    </>
+  );
+}
