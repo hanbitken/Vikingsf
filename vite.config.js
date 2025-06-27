51d2792edb5e49e695e5e57c8580f4ae51562af0
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-27 16:57:11 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: "/Vikingsf/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
